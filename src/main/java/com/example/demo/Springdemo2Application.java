125414b6dbedfb6f51e1859dca40647f47578f50
SaranyaSiripurapu <85158690+SaranyaSiripurapu@users.noreply.github.com>
2022-05-18 20:26:22 -0700

    Add files via upload

---
+++ src/main/java/com/example/demo/Springdemo2Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springdemo2Application {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello from Bridgelabz ");
+		SpringApplication.run(Springdemo2Application.class, args);
+	}
+
+}
